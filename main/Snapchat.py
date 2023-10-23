223416b85eb788e3edcafcb255144cdf44f34a8e
poop-ski <75001575+poop-ski@users.noreply.github.com>
2023-10-23 22:04:16 +0400

    Updated Snapchat.py

--- main/Snapchat.py
+++ main/Snapchat.py
@@ -1,14 +1,30 @@
-#Author : Utkarsh Yadav
-#Date : 20/10/2022
+# Author: Utkarsh Yadav
+# Date: 20/10/2022
 import pyautogui
-pyautogui.sleep(3)
-print(pyautogui.position())
+import time
+
+# Sleep initially to give you some time to focus on the Snapchat window
+time.sleep(3)
+
+# Define a function to click a specific position
+def click_position(x, y):
+    pyautogui.click(x, y)
+
+# Define a function to type a string
+def type_text(text):
+    pyautogui.write(text)
+
+# Main loop for your actions
 while True:
-    #print(pyautogui.position())
-    pyautogui.click(1012,374)
-    pyautogui.click(1095,777)
-    pyautogui.sleep(0.2)
-    pyautogui.click(1054,356)
-    pyautogui.write("<USERNAME>") #Edit this line as : pyautogui.write("utkarsh")
-    pyautogui.click(1079,513)
-    pyautogui.click(1134,805)
+    # Click on specific positions
+    click_position(1012, 395)  # Adjusted the Y-coordinate for the snap button
+    click_position(1095, 775)
+    time.sleep(0.2)
+    click_position(1054, 320)
+
+    # Type the "utkarsh" text
+    type_text("utkarsh")
+
+    # Click on more positions
+    click_position(1079, 507)
+    click_position(1134, 800)
